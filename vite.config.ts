c53a4725055514f6c98f60d5cd81e71982df4190
Babithkp <babithkullachetti44@gmail.com>
2025-01-17 22:23:07 +0530

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import gltf from 'vite-plugin-gltf';
+// import gltf from 'vite-plugin-gltf';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),gltf()],
+  plugins: [react()],
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.bin'],
 })
